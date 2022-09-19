731ff029ca3c94974a212f6df4db46ce08b1b376
Bradford Zhang <zyc@zyc.name>
2022-09-19 20:20:13 +0800

    add xmake
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

---
+++ xmake.lua
@@ -0,0 +1,8 @@
+add_rules("mode.debug", "mode.release")
+set_optimize("fastest")
+add_defines("VERSION=\"v$(env VERSION)\"", "LOG_USE_COLOR")
+
+target("log")
+    set_kind("shared")
+    add_files("log.cpp")
+
